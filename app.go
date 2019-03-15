013f9c46b6cfea3a6eba163f717e6afd18223893
Francesco Fucile <francesco.fucile.96@gmail.com>
2019-03-15 17:51:49 +0100

    'div' func created.

--- app.go
+++ app.go
@@ -4,7 +4,9 @@ import "fmt"
 
 func Div(a, b float32) (float32, error) {
 	if b == 0 {
-		return 0, fmt.Errorf("divide by 0")
+		return 0, mathError(fmt.Errorf("divide by zero"))
 	}
 	return a / b, nil
 }
+
+type mathError error
